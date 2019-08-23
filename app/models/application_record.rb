709e2952fa16e2e8fae3a51541039b27ac9aaf98
Jacob Irwin <jakobinwins@gmail.com>
2019-08-23 11:31:17 -0700

    move rescue_from method into the correct area.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = truerescue_from ActiveRecord::RecordNotFound do |exception|
-    json_response({ message: exception.message }, :not_found)
-  end
+  self.abstract_class = true
+  
 end
